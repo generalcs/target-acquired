82193d30511c7f9c84980380777b40c8319e72d3
Samuel Do <samdo@stanford.edu>
2022-11-15 22:48:29 -0800

    Set up parts for BFA2022

---
+++ rooms/scripts/next-puzzle.js
@@ -0,0 +1,38 @@
+const OK = 200;
+const NOT_FOUND = 404;
+
+function getUrlStatus(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', url, false);
+    xmlHttp.send(null);
+    return xmlHttp.status
+}
+
+function getHash(str, seed = 0) {
+    let h1 = 0xdeadbeef ^ seed,
+      h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
+function getAnswerStatus(answer, prefix) {
+    let hash = getHash(answer);
+    let url = prefix + String(hash) + '.html';
+    let status = getUrlStatus(url);
+
+    if (status == OK) {
+        window.location.href = url;
+    } else if (status == NOT_FOUND) {
+        return 'Incorrect answer!';
+    } else {
+        return 'Error: please try again.'
+    }
+}
